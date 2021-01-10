df693eb45a911d09f7516604e0d0ffeda39a3802
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-01-09 22:13:50 -0800

    practice commit

---
+++ week2/practice1.js
@@ -0,0 +1,24 @@
+var header = document.createElement("h1");
+header.textContent = "Welcome to my JS site";
+document.body.append(header);
+
+var paragraph = document.createElement("p");
+paragraph.textContent = "All of this was created with Javascript";
+document.body.append(paragraph);
+
+var list1 = document.createElement("li");
+list1.textContent = "football";
+document.body.append(list1);
+
+var list2 = document.createElement("li");
+list2.textContent = "baseball";
+document.body.append(list2);
+
+var list3 = document.createElement("li");
+list3.textContent = "basketball";
+document.body.append(list3);
+
+
+
+
+
