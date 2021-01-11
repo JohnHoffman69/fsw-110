4f2c8d28808b206a8915d902c620cf0377c1fb38
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-01-10 23:23:05 -0800

    append_elements commit

--- week2/appending_elements/practice1.js
+++ week2/appending_elements/practice1.js
@@ -1,21 +1,23 @@
+//H1//
 var header = document.createElement("h1");
 header.textContent = "Welcome to my JS site";
 document.body.append(header);
-
+//P//
 var paragraph = document.createElement("p");
 paragraph.textContent = "All of this was created with Javascript";
 document.body.append(paragraph);
 
+//ol//
+var ol = document.createElement("OL");
 var list1 = document.createElement("li");
-list1.textContent = "football";
-document.body.append(list1);
-
 var list2 = document.createElement("li");
-list2.textContent = "baseball";
-document.body.append(list2);
-
 var list3 = document.createElement("li");
-list3.textContent = "basketball";
+document.body.append(ol);
+list1.textContent = "1 football";
+list2.textContent = "2 baseball";
+list3.textContent = "3 basketball";
+document.body.append(list1);
+document.body.append(list2);
 document.body.append(list3);
 
 
---
+++ week2/website/website.js
@@ -0,0 +1,52 @@
+//navbar//
+var nav = document.createElement ("NAV");
+document.body.append(nav);
+
+//The links//
+var nav1 = document.createElement("a");
+var nav2 = document.createElement("a");
+var nav3 = document.createElement("a");
+var nav4 = document.createElement("a");
+nav1.textContent = "HOME";
+nav2.textContent = "ABOUT";
+nav3.textContent = "CONTACT";
+nav4.textContent = "LOCATION";
+
+// Links to //
+nav.appendChild(nav1);
+nav.appendChild(nav2);
+nav.appendChild(nav3);
+nav.appendChild(nav4);
+
+
+//An h1 tag//
+var H1 = document.createElement("h1");
+document.body.append(H1);
+H1.textContent = "Sports";
+
+//A p tag//
+var P = document.createElement("p");
+P.textContent = "Sports is a fun and healthy way to stay active.";
+document.body.append(P);
+
+//A list//
+var alphaList = document.createElement("ul");
+document.body.append(alphaList);
+
+//A list items//
+var alphaLi1 = document.createElement("li");
+var alphaLi2 = document.createElement("li");
+var alphaLi3 = document.createElement("li");
+alphaLi1.textContent = "Football";
+alphaLi2.textContent = "Baseball";
+alphaLi3.textContent = "Basketball";
+
+//A li to ul//
+alphaList.appendChild(alphaLi1);
+alphaList.appendChild(alphaLi2);
+alphaList.appendChild(alphaLi3);
+
+//Footer
+var Footer = document.createElement("FOOTER");
+Footer.textContent = "For more information please click at: https://www.sportsjunkie.com";
+document.body.append(Footer);
