898ab34f6ff8d8172c4cb53c787870a4710c8058
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-01-30 17:37:28 -0800

    Modifying_Elements

---
+++ week5/index.js
@@ -0,0 +1,8 @@
+document.getElementById("submit").addEventListener("click", () => {
+    var username = document.getElementById("username").value, 
+    email = document.getElementById("email").value, 
+    password = document.getElementById("password").value;
+    
+    alert(` Sign up initiated!\n ${username} taken.\n ${email} available.\n ${password} not compatible.`)
+})
+
