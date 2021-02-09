849697ba93c9f3ba63735e5f50b2b0f50acbdf80
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-02-09 14:46:14 -0800

    update

--- week6/event/click_/app.js
+++
@@ -1,5 +0,0 @@
-// make the box disapear when the user clicks it
-const box = document.querySelector('.red-box');
-const beGone = () => box.style.display = "none";
-
-box.addEventListener('click', beGone);
